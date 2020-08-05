94ed2262ade538fc49d57ec7226e23c7a6ce9458
Jason Poindexter <poindexter.json@gmail.com>
2020-08-04 19:45:19 -0700

    Replace __ with _ in message store

--- message_store.py
+++ message_store.py
@@ -3,22 +3,22 @@ import btree, json
 
 class MessageStore:
     def __init__(self, max_message_length):
-        self.__max_message_length = max_message_length  # Max amount of messages our messages array will hold
+        self._max_message_length = max_message_length  # Max amount of messages our messages array will hold
         self.messages = []  # Array of message objects:  { 'timestamp': number, 'sender': string, 'message': string }
-        self.__file = None
-        self.__db = None
-        self.__load_messages_from_db()
+        self._file = None
+        self._db = None
+        self._load_messages_from_db()
 
     # Load messages from persistent storage into messages array
     # TODO: handle MAX_MESSAGES_LENGTH change
-    def __load_messages_from_db(self):
+    def _load_messages_from_db(self):
         try:
-            self.__file = open("messages.db", "r+b")
+            self._file = open("messages.db", "r+b")
         except OSError:
             print('[BTREE] OSError')
-            self.__file = open("messages.db", "w+b")
-        self.__db = btree.open(self.__file)
-        for messageStr in self.__db.values():
+            self._file = open("messages.db", "w+b")
+        self._db = btree.open(self._file)
+        for messageStr in self._db.values():
             try:
                 self.messages.append(json.loads(messageStr))
             except Exception as error:
@@ -27,18 +27,18 @@ class MessageStore:
     # Add message to messages array and persistent data
     def add_message(self, message):
         try:
-            if len(self.messages) >= self.__max_message_length:  # Make sure local messageObj array size is constrained
+            if len(self.messages) >= self._max_message_length:  # Make sure local messageObj array size is constrained
                 popped = self.messages.pop(0)  # Pop oldest message from messageObj
-                del self.__db[str(popped['timestamp']).encode()]  # Remove message from message db
-                self.__db.flush()
+                del self._db[str(popped['timestamp']).encode()]  # Remove message from message db
+                self._db.flush()
             self.messages.append(message)  # Add to local messageObj array
-            self.__db[str(message['timestamp']).encode()] = json.dumps(
+            self._db[str(message['timestamp']).encode()] = json.dumps(
                 message)  # Add new message to db using [key: messageObj.timestamp, value: messageObj
-            self.__db.flush()
+            self._db.flush()
         except Exception as error:
             print('[addMessage] ', error)
 
     # Close everything we going home
     def close(self):
-        self.__db.close()
-        self.__file.close()
+        self._db.close()
+        self._file.close()
